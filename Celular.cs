031436959a426adc97c771460fe439b3a46907d3
ViniciusMirandaDev <ViniciusMirandaDev>
2020-05-27 17:58:21 -0300

    Finalizado

--- Celular.cs
+++ Celular.cs
@@ -4,45 +4,47 @@ namespace Aula3___Sprint4___POOLicçao
 {
     public class Celular
     {
-        // Atributos
+        //Atributos
         public string cor;
         public string modelo;
         public float tamanho;
         public bool ligado;
 
         //Métodos
+
         /// <summary>
         /// Ligar o celular
         /// </summary>
         /// <returns>Texto de celular ligado</returns>
         public string Ligar(){
-            ligado=true;
-            return "Celular ligado";
+            ligado = true;
+            return "Celular ligando";
         }
+
         /// <summary>
         /// Desligar o celular
         /// </summary>
         /// <returns>Texto de celular desligado</returns>
         public string Desligar(){
             ligado=false;
-            return "Celular desligando";
+            return "Celular desligado";
         }
 
         /// <summary>
-        /// reaiza ligação pelo celular
+        /// Realiza a ligação pelo celular
         /// </summary>
         /// <returns>Texto de ligação efetuada</returns>
         public string FazerLigacao(){
             return "Realizando ligação";
         }
 
-        /// <summary>
-        /// Envia mensagem
-        /// </summary>
-        /// <returns>Texto de mensagem enviada</returns>
+        
         public string EnviarMensagem(){
             return "Enviando mensagem";
         }
 
     }
+
+
+    
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,44 @@ namespace Aula3___Sprint4___POOLicçao
     {
         static void Main(string[] args)
         {
-            Celular motorola = new Celular();
+             
+            Celular iphone = new Celular();
+            iphone.modelo = "X";
+            iphone.tamanho = 10f;
+
+            Console.WriteLine ("Deseja ligar o celular?");
+            iphone.ligado = Boolean.Parse(Console.ReadLine());
+
+            if(iphone.ligado){
+
+                while(iphone.ligado){
+
+                    Console.WriteLine("O que desja fazer? \n 1-Fazer ligação \n 2-Enviar Mensagem \n 3-Desligar");
+                    int resposta = Int32.Parse(Console.ReadLine());
+
+                    switch(resposta){
+
+                        case 1:
+                        Console.WriteLine( iphone.FazerLigacao() );
+                        break;
+
+                        case 2:
+                        Console.WriteLine( iphone.EnviarMensagem() );
+                        break;
+
+                        case 3:
+                        Console.WriteLine( iphone.Desligar() );
+                        break;
+
+                        default:
+                        Console.WriteLine( iphone.Ligar() );
+                        break;
+
+                    }
+                }
+            }else{
+                Console.WriteLine("O celular está deligado!");
+            }
         }
     }
 }
